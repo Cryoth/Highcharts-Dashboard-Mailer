253f22f0af5b37d5e9c6ab24baced562c3e28d59
Guillaume SCHNEYDER <g.schneyder@laposte.net>
2018-05-22 16:33:44 +0200

    Mise en place de promesses et et ajout module de serialization php

--- config/request.js
+++ config/request.js
@@ -3,24 +3,58 @@ var db = require('./db.js');
 module.exports = {
 
 	getListDestinataire: function(params){
-		database = db(params);
-		database.query('SELECT id, Login, Email FROM Client WHERE SendDashboard = 1', function(err, result) {
-	      if (err) throw err;
-	      console.log(result);
-	      return result;
-	    });
+		
+		return new Promise(function(resolve, reject){
+			database = db(params);
+			database.query('SELECT id, Login, Email FROM Client WHERE SendDashboard = 1', function(err, result) {
+		      if (err) 
+		      	return reject(err);
+		      else
+		      	resolve(result);
+		    });
 
-	    database.end();
+		    database.end();
+		});
+
 	},
 
 	getChartsToSend: function(params){
-		database = db(params);
-		database.query('SELECT DISTINCT(`idModele`) FROM `Client_Modele` INNER JOIN Client ON Client_Modele.idClient = Client.id WHERE SendDashboard = 1', function(err, result) {
-	      if (err) throw err;
-	      console.log(result);
-	      return result;
-	    });
 
-	    database.end();
+		return new Promise(function(resolve, reject){
+			database = db(params);
+			database.query('SELECT DISTINCT(`idModele`) AS Id, Nom, FormeGraph, Periode  FROM `Modele` INNER JOIN Client_Modele ON Modele.id = Client_Modele.idModele INNER JOIN Client ON Client_Modele.idClient = Client.id WHERE SendDashboard = 1', function(err, result) {
+		      if (err) 
+		      	return reject(err);
+		      else
+		      	resolve(result);
+		    });
+
+		    database.end();
+		});
+
+	},
+
+	getChartSeries: function(params, chartId){
+
+		return new Promise(function(resolve, reject){
+			database = db(params);
+			database.query('SELECT * FROM Identificateur_Modele WHERE Modele_id = ?', [chartId], function(err, result) {
+		      if (err) 
+		      	return reject(err);
+		      else
+		      	resolve(result);
+		    });
+
+		    database.end();
+		});
+
+	},
+
+	getSeriesData: function(params){
+		database = db(params);
+		database.query('', [chartId], function(err, result){
+			if (err) throw err;
+			return result;
+		});
 	},
 }
--- index.js
+++ index.js
@@ -10,24 +10,54 @@ const mailer = require('./lib/mailer.js');
 // Convertisseur au format JSON exploitable par Highcharts
 const formatJSON = require('./lib/json-formatter.js');
 
+// Serializer pour données depuis php
+const PHPUnserialize = require('php-unserialize');
+
 // Liste des requêtes aux serveurs BDD
 var DB = require('./config/request.js');
 
 
+
+// Annonce le mode développement si celui-ci est de rigueur
 if(env == 'development'){
 	console.log('\n===============  MODE DEVELOPMENT  ===============\n');
 }
 
 
 
+// Parcours les BDD de Check Anywhere
 config.CheckAnywhere.forEach(function(db, index){
 
-	var clients = DB.getListDestinataire(db); // Inclus les id de graphiques à envoyer
-	var charts = DB.getChartsToSend(db);
+	// Récupération de la liste des destinataires
+	var promiseClients = DB.getListDestinataire(db);
 
-	for (chart in charts){
-		
-	}
+	// Récupération de la liste des graphiques
+	var promiseCharts = DB.getChartsToSend(db);
+
+	// Execute ce code une fois les deux éléments du dessus récupérés
+	Promise.all([promiseClients, promiseCharts]).then(function([clients, charts]) {
+  		
+  		console.log("Clients et Graphs OK");
+
+  		charts.forEach(function(chart){
+
+  			DB.getChartSeries(db, chart["Id"]).then(function(series){
+
+  				series.forEach(function(serie){
+
+  					valeur = PHPUnserialize.unserialize(serie.Valeurs);
+
+  				});
+
+  			});
+
+  		});
+
+	}).catch(function(err){
+
+		console.log(err);
+
+	});
 
 	// clients.forEach(function(client){
 	// 	var mail =  mailer.prepare(client);
